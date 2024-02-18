9664937d4e1a587a62f4032bc646bfa159321938
AmiltonRocha <Amiltonrholanda@gmail.com>
2024-02-18 19:41:13 -0300

    Exercicio Beecrowd 1004

--- Ex1004/Main.java
+++ Ex1004/Main.java
@@ -8,6 +8,6 @@ public class Main {
        Scanner int2 = new Scanner(System.in);
        int valor2 = int2.nextInt();
        int PROD = valor1 * valor2;
-       System.out.printf("O valor de %d * %d e igual a %d ",valor1,valor2,PROD);
+       System.out.printf("PROD = %d",PROD);
     }
 }
