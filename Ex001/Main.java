08d5d7e7859494c38aa3d3411eb742f3bc6fa5c5
AmiltonRocha <Amiltonrholanda@gmail.com>
2024-02-19 15:00:35 -0300

    Beecrowd 1002

--- Ex001/Main.java
+++ Ex001/Main.java
@@ -7,9 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double raio = scanner.nextDouble();
+        scanner.close();
         double n = 3.14159;
         double area = n * (raio * raio);
-        System.out.format("A =%.4f ", area);
+        System.out.format("A=%.4f/n", area);
 
     }
 }
